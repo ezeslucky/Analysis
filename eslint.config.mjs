e882bfeaf8e5c5b22f8d6870bdc6fe8a157c6065
Lucky <lucky123@gmail.com>
2025-03-19 09:26:13 +0530

    yarn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,20 +9,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = {
-  ...compat.config({
-    extends: [
-      'next/core-web-vitals',
-       'next/typescript', 
-       'prettier',
-       'plugin: tailwindcss/recommended'
-      ],
-    plugins: ['prettier'],
-    rules:{
-      'prettier/prettier': 'error',
-      'react/no-escape-entities': 'off'
-    },
-  }),
-}
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
 
 export default eslintConfig;
